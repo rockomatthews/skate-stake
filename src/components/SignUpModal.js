67600c209a8d9a5751c7be63b524c4bb82387c0f
rockomatthews <rob@robmatthe.ws>
2023-12-18 19:43:47 -0600

    Added metadata to singup modal to progmaticaly provide new users with a skater and skatebaord

--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -51,8 +51,83 @@ function SignUpModal({ isOpen, onClose }) {
         },
         body: JSON.stringify({
           referenceId,
-          skaterMetadata: {/* Skater metadata here */},
-          skateboardMetadata: {/* Skateboard metadata here */}
+          skaterMetadata: {
+            "name": "Skater00",
+            "description": "Skater00 tester",
+            "sourceImage": "https://firebasestorage.googleapis.com/v0/b/skate-stake.appspot.com/o/skater00.gif?alt=media&token=2bb8ce62-bc0d-48f5-8d3f-efc93b5218c6",
+            "collectionId": "e6c74a89-6a2d-4acf-a7b4-f79e7bb56f32",
+            "attributes": [
+              {
+                "trait_type": "Attitude",
+                "value": "Interested"
+              },
+              {
+                "trait_type": "Adrenaline Boost",
+                "value": "00%"
+              },
+              {
+                "trait_type": "Age",
+                "value": "14"
+              },
+              {
+                "trait_type": "Stamina",
+                "value": "Low"
+              },
+              {
+                "category": "Flips",
+                "traits": [
+                  {
+                    "trait_type": "Backflip Success Rate",
+                    "value": "00%"
+                  }
+                ]
+              },
+              {
+                "category": "Spins",
+                "traits": [
+                  {
+                    "trait_type": "180 Spin Success Rate",
+                    "value": "00%"
+                  }
+                ]
+              },
+              {
+                "category": "Board Flips",
+                "traits": [
+                  {
+                    "trait_type": "Kickflip Success Rate",
+                    "value": "00%"
+                  }
+                ]
+              },
+              {
+                "category": "Grabs",
+                "traits": [
+                  {
+                    "trait_type": "Nosegrab Success Rate",
+                    "value": "00%"
+                  }
+                ]
+              }
+            ]
+        },
+          skateboardMetadata: {
+            "name": "Skateboard00",
+            "description": "A free skateboard",
+            "sourceImage": "https://firebasestorage.googleapis.com/v0/b/skate-stake.appspot.com/o/skateboard00.gif?alt=media&token=48cfe199-369b-42b7-bab7-f3f85cdd091e",
+            "collectionId": "3e0bd7ea-38ad-4674-bfab-a726b5561385",
+            "attributes": [
+              {
+                "trait_type": "Learning Multiplier",
+                "value": "1"
+              },
+              {
+                "trait_type": "Speed",
+                "value": "1"
+              }
+            ]
+        }
+                
         })
       });
 
